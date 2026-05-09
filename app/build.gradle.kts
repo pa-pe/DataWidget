bab9ba0ff075de7d92d6a3eb7833fb742cd77d02
Pavel <pavel@p-p.name>
2026-05-08 21:26:49 -0400

    Fix widget buttons size with custom background

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         applicationId = "name.xoid.datawidget"
         minSdk = 27
         targetSdk = 36
-        versionCode = 26
+        versionCode = 27
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
